059ecda20750586a8c52b82b667569d872b71ffb
zyt76 <zyt768295828@163.com>
2017-11-06 00:34:03 +0800

    首页重写3

--- JS/main.js
+++ JS/main.js
@@ -3,6 +3,9 @@ window.onload = function () {
     var oHeader = document.getElementsByTagName('header')[0];
     var clientY = document.documentElement.clientHeight;
     var oMain = document.getElementById("main");
+    var oBaseInfo = document.getElementById("base_info");
+    var scrollTarget = clientY - (oBaseInfo.children.length + 1) * 23 - 34;
+    var timer = null;
     oMain.style.marginTop = clientY + 'px';
     // console.log(oMain.style.top);
     oAside.timer = null;
@@ -23,6 +26,25 @@ window.onload = function () {
                 }
                 oAside.timer = setTimeout(go, 10);
             }
+            if (timer === null && document.documentElement.scrollTop == 0) {
+                var scrollTemp = scrollTarget;
+                var scrollNow = 0;
+                var scroll = function scroll() {
+                    scrollNow = (scrollTarget - scrollNow) * 0.05 + 1;                    
+                    document.documentElement.scrollTop += scrollNow;
+                    console.log(document.documentElement.scrollTop);
+                    clearTimeout(timer);
+                    timer = null;
+                    if (document.documentElement.scrollTop >= scrollTarget) {
+                        document.documentElement.scrollTop = scrollTarget;
+                        return false;
+                    }
+                    timer = setTimeout(scroll, 10);
+                    return false;
+                }
+                timer = setTimeout(scroll, 10);
+            }
+            return false;
         } else if (parseInt(getComputedStyle(oAside).top) == -clientY && e.wheelDelta > 0) {
             if (oAside.timer === null) {
                 var go = function go() {
@@ -38,6 +60,7 @@ window.onload = function () {
                 }
                 oAside.timer = setTimeout(go, 10);
             }
+            return false;
         }
     }
 }
--- index.php
+++ index.php
@@ -56,22 +56,46 @@
                 mysqli_close($connection);
             ?>
         </aside>
-        <div class="base_info">
+        <div class="base_info" id="base_info">
             <code>WELCOME TO ZYT76'S BLOG</code>
             <code>==============================bio==============================</code>
             <code>blogger: zyt76;</code>
             <code>email: zyt768295828@163.com;</code>
             <code>phone: 13167085630;</code>
-            <code></code>
-            <code></code>
-            <code></code>
-            <code></code>
-            <code></code>
-            <code></code>
-            <code></code>
         </div>
         <div class="main" id="main">
-
+            <div class="header">
+                <div>
+                    <span>Client</span>
+                    <span>ESPN</span>
+                </div>
+                <div>
+                    <span>
+                        Case
+                        <span></span>
+                    </span>
+                    <h2>
+                        <span>ESPN</span>
+                        <span>We The Fans</span>
+                    </h2>
+                </div>
+                <div></div>
+                <div></div>
+            </div>
+            <div class="subhead">
+                <h3>
+                We The Fans is a multiplatform storytelling project about life and love as a football fan. In collaboration with ESPN, we created a digital experience exploring the lives of Chicago Bears diehards.
+                </h3>
+                <div></div>
+            </div>
+            <div class="content">
+                <div>
+                    <p>Essential’s products are new, but the story is ancient. It would of course be extremely strange to compare it to the movie 300. So let’s do that. While King Leonidas is leading 300 spartans with Herculean bodies and sharp blades into the battle against the Persian army of 300.000 soldiers, Andy Rubin is leading 100 tech superstars with insane skills and sharp brains into an epic consumer tech battle. The Hollywood version is admittedly a bit more dramatic, but the people are essential. They are the difference between winning and losing.</p>
+                    <p>This is why we decided to include them in the narrative and merge the qualities of a product site with the storytelling aspects of a blog. We literally made a rule that we’d never show glossy products without sharing the thoughts and the people behind them. We made 100 people the heroes on Essential.com because they are. Secondly, tech companies can easily become faceless giants, and Essential is a counteraction to this approach. The company is not about more or bigger. It’s about better.</p>
+                    <p>We’re currently working with Essential to make Essential.com even better and prepare it for the launch of an intelligent home device. In other words, we’re doing everything we can to help Andy and his outnumbered team prove that it’s possible to make a big impact even though (and exactly because) they’re not big.</p>
+                </div>
+                <div></div>
+            </div>
         </div>
         <footer>
             
